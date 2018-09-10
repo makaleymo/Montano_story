507dde0ced38d2d84a85f4b01c02f2e2744ebdf9
Makaley Montano <makaleymon.81979@redlandsschools.net>
2018-09-10 15:16:20 +0000

    intial commit

---
+++ first.py
@@ -0,0 +1,7 @@
+# Write a program that will add six to the users input and return the result!
+print ("hello world")
+
+age=int(input("how old are you?"))
+print (2*age)
+
+print ("Wow, you are" + str(age) + "I")
